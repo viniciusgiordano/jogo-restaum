9817e03cbcceacac91b16d740256160d8d6a549f
Vinicius Giordano <giordano.vinicius12@gmail.com>
2022-06-23 22:04:04 -0300

    correção

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = process.env.PORT || 8080
 
 // sendFile will go here
 app.use(express.static(__dirname))
-app.use('/', require('js/app.js'))
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
 
 app.listen(port)
 console.log('Server started at http://localhost:' + port)
